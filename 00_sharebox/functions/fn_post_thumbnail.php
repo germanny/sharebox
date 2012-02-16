fd4378bdac55577e36fa3a5da5510d9735fa2e91
Jen G <germanny@gmail.com>
2012-02-16 17:50:58 -0600

    updated finding any attached image to use thumbnail

--- 00_sharebox/functions/fn_post_thumbnail.php
+++ 00_sharebox/functions/fn_post_thumbnail.php
@@ -1,7 +1,7 @@
 <?php
 function jg_post_thumbnail_src($size = thumbnail) { //get the src of first image
 	global $post;
-		$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
 	$first_img = $matches [1] [0];
 	$custom_image = get_post_meta($post->ID, 'custom_image_value', TRUE);
 	if (has_post_thumbnail( $post->ID ) ) { 
@@ -15,16 +15,16 @@ function jg_post_thumbnail_src($size = thumbnail) { //get the src of first image
   	'numberposts'    => 1,
 	))) {
 		foreach($images as $image) {
-			$atturl   = wp_get_attachment_url($image->ID); // Get attachment image URL
+			$atturl   = wp_get_attachment_image_src($image->ID, $size); // Get attachment thumbail image URL
 		}
-  }
+	}
 	
 	if ($featured_image) { // author sets a specific Featured Image
 		$the_first_image = $featured_image[0];
 	} elseif ($custom_image) { // theme has a Custom Field for "custom_image_value" and author sets the url
 		$the_first_image = $custom_image; 
 	} elseif ($atturl) { // author has uploaded various image attachments, and may or may not have put them in the content
-		$the_first_image = $atturl; 
+		$the_first_image = $atturl[0]; 
 	} elseif ($first_img) { // author put an image in the content area that is not an attachment (???)
 		$the_first_image = $first_img; 
 	} else { // there just isn't any images anywhere in the post, and a default image is what we need
